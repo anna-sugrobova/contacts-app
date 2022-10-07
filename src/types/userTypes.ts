6b1931cc5b57fe08b039c024ff8a47b3bbeeb7ed
Anna Sugrobova <a.sugrobova@pik-remont.ru>
2022-10-08 00:48:12 +0700

    fixes for UserDataType

--- src/types/userTypes.ts
+++ src/types/userTypes.ts
@@ -1,19 +1,19 @@
 export type UserDataType = {
-    id: string,
-    name: {title: string, first: string, last: string},
-    gender: string,
-    location: { city: string, country: string, postcode: string },
-    picture: {large: string},
-    email: string,
-    phone: string,
-}
+  id: string;
+  name: string;
+  gender: string;
+  location: { city: string; country: string; postcode: string };
+  picture: { large: string };
+  email: string;
+  phone: string;
+};
 
 export type UserDataFromApiType = {
-    id: {name: string, value: string},
-    name: {title: string, first: string, last: string},
-    gender: string,
-    location: { city: string, country: string, postcode: string },
-    picture: {large: string},
-    email: string,
-    phone: string,
-}
+  id: { name: string; value: string };
+  name: { title: string; first: string; last: string };
+  gender: string;
+  location: { city: string; country: string; postcode: string };
+  picture: { large: string };
+  email: string;
+  phone: string;
+};
