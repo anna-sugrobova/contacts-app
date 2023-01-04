bc4f32f965b2f5e40002a540a417eb576637fadb
Anna Sugrobova <a.sugrobova@pik-remont.ru>
2023-01-04 22:11:08 +0100

    add error handling for Login & Contact pages

--- src/screens/ContactsPage/ContactsPage.tsx
+++ src/screens/ContactsPage/ContactsPage.tsx
@@ -1,27 +1,28 @@
-import { useState, useEffect, FC } from "react";
-import { User } from "../../components/User/User";
-import uniqueId from "lodash/uniqueId";
-import { MAIN_URL, SEARCH_QUERY } from "../../api/api";
-import { setUsersData } from "../../redux/userSlice";
-import { useAppDispatch, useAppSelector } from "../../redux/hooks";
-import { UserDataFromApiType } from "../../types/userTypes";
-import useModal from "../../hooks/useModal";
-import Modal from "../../components/Modal/Modal";
-import { EditUserModal } from "../../components/EditUserModal/EditUserModal";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import SvgIcon from "@mui/material/SvgIcon";
-import { useNavigate } from "react-router-dom";
-import "./ContactsPage.scss";
-import {Spinner} from "../../components/Spinner/Spinner";
+import { FC, useEffect, useState } from 'react';
+import { User } from '../../components/User/User';
+import uniqueId from 'lodash/uniqueId';
+import { MAIN_URL, SEARCH_QUERY } from '../../api/api';
+import { setUsersData } from '../../redux/userSlice';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { UserDataFromApiType } from '../../types/userTypes';
+import useModal from '../../hooks/useModal';
+import Modal from '../../components/Modal/Modal';
+import { EditUserModal } from '../../components/EditUserModal/EditUserModal';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import SvgIcon from '@mui/material/SvgIcon';
+import { useNavigate } from 'react-router-dom';
+import './ContactsPage.scss';
+import { Spinner } from '../../components/Spinner/Spinner';
 
 export const ContactsPage: FC = () => {
   const dispatch = useAppDispatch();
   const { isShowing, toggleModal } = useModal();
   let navigate = useNavigate();
   const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
-    (async function() {
+    (async function () {
       try {
         setIsFetching(true);
         const requestUrl = `${MAIN_URL}?${SEARCH_QUERY}`;
@@ -40,17 +41,18 @@ export const ContactsPage: FC = () => {
             name: fullName,
             location: fullLocation,
           };
-        })
+        });
         dispatch(setUsersData(formattedUsers));
         setIsFetching(false);
       } catch (error) {
         console.log(error);
+        setError(true);
       }
-    })()
+    })();
   }, [dispatch]);
 
   const users = useAppSelector((state) => state.contacts.users);
-  const [userIdToEdit, setUserIdToEdit] = useState("");
+  const [userIdToEdit, setUserIdToEdit] = useState('');
 
   const editHandler = (id: string) => {
     toggleModal();
@@ -58,7 +60,7 @@ export const ContactsPage: FC = () => {
   };
 
   const handleBackButtonClick = () => {
-    navigate("/home");
+    navigate('/');
   };
 
   const countOfUsersToRender = 20;
@@ -66,39 +68,53 @@ export const ContactsPage: FC = () => {
 
   return (
     <>
-      { isFetching && <Spinner />}
-          <div className="contacts-page-container">
-            <header className="contacts-page-header">
-              <button type="button" onClick={handleBackButtonClick} className="back-button">
-                <SvgIcon className="arrow-icon">
-                  <ArrowBackIcon />
-                </SvgIcon>
-              </button>
-              <h1 className="contact-page-title">Contacts</h1>
-            </header>
-            <div className="users-container">
-              {usersToRender.map((user: any) => {
-                const { id, name, gender, location, email, phone, picture: { large } } = user;
+      {isFetching && <Spinner />}
+      {error ? (
+        <div className="errors">
+          Something went wrong &#128530;<br></br> Please, try again later
+        </div>
+      ) : (
+        <div className="contacts-page-container">
+          <header className="contacts-page-header">
+            <button type="button" onClick={handleBackButtonClick} className="back-button">
+              <SvgIcon className="arrow-icon">
+                <ArrowBackIcon />
+              </SvgIcon>
+            </button>
+            <h1 className="contact-page-title">Contacts</h1>
+          </header>
+          <div className="users-container">
+            {usersToRender.map((user: any) => {
+              const {
+                id,
+                name,
+                gender,
+                location,
+                email,
+                phone,
+                picture: { large },
+              } = user;
 
-                return (
-                    <User
-                        id={id}
-                        gender={gender}
-                        key={id}
-                        name={name}
-                        src={large}
-                        email={email}
-                        phone={phone}
-                        location={location}
-                        onEdit={editHandler}
-                    />
-                );
-              })}
-              <Modal isShowing={isShowing} hide={toggleModal}>
-                <EditUserModal userIdToEdit={userIdToEdit} closeModal={toggleModal} />
-              </Modal>
-            </div>
+              return (
+                <User
+                  id={id}
+                  gender={gender}
+                  key={id}
+                  name={name}
+                  src={large}
+                  email={email}
+                  phone={phone}
+                  location={location}
+                  onEdit={editHandler}
+                />
+              );
+            })}
+            <Modal isShowing={isShowing} hide={toggleModal}>
+              <EditUserModal userIdToEdit={userIdToEdit} closeModal={toggleModal} />
+            </Modal>
           </div>
+        </div>
+      )}
     </>
   );
 };
--- src/screens/loginPage/LoginPage.tsx
+++ src/screens/loginPage/LoginPage.tsx
@@ -1,57 +1,71 @@
-import { SyntheticEvent, FC } from 'react';
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import background from "../../assets/background.jpg";
-import "./LoginPage.scss";
+import { FC, SyntheticEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import background from '../../assets/background.jpg';
+import './LoginPage.scss';
 
 export const LoginPage: FC = () => {
+  const [error, setError] = useState(false);
   const navigate = useNavigate();
 
+  const hideError = () => setError(false);
+  const handleError = (error: Error) => {
+    console.log(error);
+    setError(true);
+  };
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
-    try {
-      axios({
-        method: "GET",
-        url: "http://localhost:3000/login",
-      }).then((response) => {
+
+    axios({
+      method: 'GET',
+      url: 'http://localhost:3000/login',
+    })
+      .then((response) => {
         if (response.data.success) {
-          navigate("/contacts");
+          navigate('/contacts');
         }
+      })
+      .catch((error) => {
+        handleError(error);
       });
-    } catch (e) {
-      console.log(e);
-    }
   };
 
   return (
-    <div className="page">
-      <div className="login-wrapper">
-        <img src={background} alt="Login form background" />
-        <div className="form-wrapper">
-          <form className="login-form" onSubmit={handleSubmit}>
-            <h1 className="form-header">Welcome</h1>
-            <div>
-              <input
-                className="form-input login"
-                type="email"
-                placeholder="Email"
-                required
-              />
-            </div>
-            <div>
-              <input
-                className="form-input password"
-                type="password"
-                placeholder="Password"
-                required
-              />
-            </div>
-            <div>
-              <input type="submit" value="Login" className="login-button" />
-            </div>
-          </form>
+    <>
+      <div className="page">
+        <div className="login-wrapper">
+          <img src={background} alt="Login form background" />
+          <div className="form-wrapper">
+            <form className="login-form" onSubmit={handleSubmit}>
+              <h1 className="form-header">Welcome</h1>
+              <div>
+                <input
+                  className="form-input login"
+                  type="email"
+                  placeholder="Email"
+                  required
+                  onFocus={hideError}
+                />
+              </div>
+              <div>
+                <input
+                  className="form-input password"
+                  type="password"
+                  placeholder="Password"
+                  required
+                  onFocus={hideError}
+                />
+              </div>
+              <div>
+                <input type="submit" value="Login" className="login-button" onFocus={hideError} />
+                {error && (
+                  <div className="login-error">Login failed. Invalid email or password.</div>
+                )}
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
