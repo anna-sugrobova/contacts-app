72db4c0f36d42dd46123490994ab8da6c920ceca
Anna Sugrobova <a.sugrobova@pik-remont.ru>
2023-01-12 19:39:44 +0100

    add initial avatar for a new user without uploaded photo & improve card styles

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -7,6 +7,7 @@ import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import PhoneIcon from '@mui/icons-material/Phone';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import SvgIcon from '@mui/material/SvgIcon';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import './Contact.scss';
 
 interface ContactProps {
@@ -29,29 +30,37 @@ export const Contact: FC<ContactProps> = ({ id, src, name, location, email, phon
 
   return (
     <div className="card-wrapper">
-      <div className="photo-wrapper">
-        <img src={src} alt="Profile background" className="contact-image" />
-      </div>
-      <CloseButton onClick={handleDeleteContact} />
-      <div className="personal-description">
-        <p className="contact-name">{name}</p>
-        <div className="contact-data-container">
-          <SvgIcon>
-            <PhoneIcon />
-          </SvgIcon>
-          <p className="contact-data-text">{phone}</p>
+      <div className="content-wrapper">
+        <div className="photo-wrapper">
+          {src ? (
+            <img src={src} alt="Profile background" className="contact-image" />
+          ) : (
+            <SvgIcon className="avatar-icon">
+              <AccountCircleOutlinedIcon />
+            </SvgIcon>
+          )}
         </div>
-        <div className="contact-data-container">
-          <SvgIcon>
-            <MailOutlineIcon />
-          </SvgIcon>
-          <p className="contact-data-text">{email}</p>
-        </div>
-        <div className="contact-data-container">
-          <SvgIcon>
-            <LocationOnIcon />
-          </SvgIcon>
-          <p className="contact-data-text">{location}</p>
+        <CloseButton onClick={handleDeleteContact} />
+        <div className="personal-description">
+          <p className="contact-name">{name}</p>
+          <div className="contact-data-container">
+            <SvgIcon>
+              <PhoneIcon />
+            </SvgIcon>
+            <p className="contact-data-text">{phone}</p>
+          </div>
+          <div className="contact-data-container">
+            <SvgIcon>
+              <MailOutlineIcon />
+            </SvgIcon>
+            <p className="contact-data-text">{email}</p>
+          </div>
+          <div className="contact-data-container">
+            <SvgIcon>
+              <LocationOnIcon />
+            </SvgIcon>
+            <p className="contact-data-text">{location}</p>
+          </div>
         </div>
       </div>
       <Button onClick={() => onEdit(id)} value="Edit" isIcon>
